4f33b9711015fb7ee141c113a39853e50c0c81a4
markhannon <mark.hannon@gmail.com>
2025-04-28 10:05:30 +1000

    TyreCompound typo and nil values for TyreOptimalTempExtension
    * Fix typo for TyreCompound
    
    * if no compuindName is sent then return nil for max temps (as all other data is also nil)

--- Assetto Corsa/apps/lua/SimHubUDPConnector/extensions/TyreOptimalTempExtension.lua
+++ Assetto Corsa/apps/lua/SimHubUDPConnector/extensions/TyreOptimalTempExtension.lua
@@ -27,7 +27,7 @@ local wheelsState = carState.wheels ---@type ac.StateWheel
 local function maximumOptimalTemperature(compoundName, minTemp)
 	if (compoundName == nil) then
 		-- in replays those values are nil
-		return minTemp and (minTemp + 30) or 80
+		return nil
 	end
 	if string.find(compoundName, "Street") then
 		return minTemp + 10
@@ -54,7 +54,7 @@ function TyreOptimalTempExtension:update(dt, customData)
 	customData.IdealPressureFront = carsUtils.getTyreConfigValue(carState.compoundIndex, true, "PRESSURE_IDEAL", 0)
 	customData.IdealPressureRear = carsUtils.getTyreConfigValue(carState.compoundIndex, false, "PRESSURE_IDEAL", 0)
 	customData.MinimumOptimalTemperature = wheelsState[0].tyreOptimumTemperature
-	customData.MaximumOptimalTemperature = maximumOptimalTemperature(customData.tyreCompound,
+	customData.MaximumOptimalTemperature = maximumOptimalTemperature(customData.TyreCompound,
 		customData.MinimumOptimalTemperature)
 	if ac.getPatchVersionCode() >= 3334 then
 		-- if (cspVersion:versionCompare("0.2.7") > -1) then
