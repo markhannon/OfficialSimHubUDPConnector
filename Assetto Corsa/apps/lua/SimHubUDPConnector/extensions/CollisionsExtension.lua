0d14f0ef9afc144130bbe4ea42b3aab3bf54149f
Dasde <dasde@free.fr>
2025-04-30 16:48:08 +0200

    Added ColliderSpeed to the CollisionExtension

--- Assetto Corsa/apps/lua/SimHubUDPConnector/extensions/CollisionsExtension.lua
+++ Assetto Corsa/apps/lua/SimHubUDPConnector/extensions/CollisionsExtension.lua
@@ -19,6 +19,7 @@ function CollisionsExtension:update(dt, customData)
     customData.CollidedWithId = carState.collidedWith
     customData.CollidedWith = (carState.collidedWith == 0) and 'Track' or
     ((carState.collidedWith > 0) and ac.getDriverName(carState.collidedWith) or 'None')
+    customData.ColliderSpeed = (carState.collidedWith > 0) and ac.getCar(carState.collidedWith).speedKmh or 0
 end
 
 return CollisionsExtension
