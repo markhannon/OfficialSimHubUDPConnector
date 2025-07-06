f28a0abc7272e0ddfd8ea9e48ab2c0a3efdca9e3
Mark Hannon <mark.hannon@gmail.com>
2025-07-06 16:40:33 +1000

    Version for debug with explicit position

--- Assetto Corsa/apps/lua/SimHubUDPConnector/extensions/LeaderBoardExtension.lua
+++ Assetto Corsa/apps/lua/SimHubUDPConnector/extensions/LeaderBoardExtension.lua
@@ -21,44 +21,35 @@ for i, c in ac.iterateCars.leaderboard() do
 	pitTimer[c.index] = 0
 end
 
-local function toIndex(i)
+local function _idx(i)
 	return string.format("%02d",tostring(i))
 end
 
 function LeaderBoardExtension:update(dt, customData)
-
 	for i, c in ac.iterateCars.leaderboard() do
-
-		if (c.isConnected == true) then
-			customData["Position_" .. toIndex(i) .. "_IsConnected"] = true
-			customData["Position_" .. toIndex(i) .. "_DriverName"] = ac.getDriverName(c.index)
-			if (c.isInPit == true and pitTimer[c.index] == 0) then
-				ac.console(ac.getDriverName(c.index) .. " entering the pits")
-				lastPitLap[c.index] = c.lapCount
-			end
-			if (c.isInPit == true) then
-				pitTimer[c.index] = pitTimer[c.index] + dt
-			end
-			if (c.isInPit ~= true and pitTimer[c.index] ~= 0) then
-				lastPitTime[c.index] = pitTimer[c.index]
-				pitTimer[c.index] = 0
-				if (c.lapCount > 0) then
-					ac.console(ac.getDriverName(c.index) .. " leaving the pits")
-					-- customData["Position_" .. toIndex(i) .. "_LastPitTime"] = tonumber(string.format("%.1f", lastPitTime[c.index]))
-					customData["Position_" .. toIndex(i) .. "_LastPitTime"] = lastPitTime[c.index]
-					customData["Position_" .. toIndex(i) .. "_LastPitLap"] = lastPitLap[c.index]
-				end
-			end
-			customData["Position_" .. toIndex(i) .. "_IsInPit"] = c.isInPit
-			customData["Position_" .. toIndex(i) .. "_LapCount"] = c.lapCount
-			customData["Position_" .. toIndex(i) .. "_Number"] = ac.getDriverNumber(c.index)
-			customData["Position_" .. toIndex(i) .. "_TyreCompound"] = ac.getTyresName(c.index, c.compoundIndex)
-		else
-			customData["Position_" .. toIndex(i) .. "_IsConnected"] = false
+		customData["Position_" .. _idx(i) .. "_BestLapTimeMs"] = c.bestLapTimeMs
+		customData["Position_" .. _idx(i) .. "_RacePosition"] = c.racePosition
+		customData["Position_" .. _idx(i) .. "_IsConnected"] = c.isConnected
+		customData["Position_" .. _idx(i) .. "_DriverName"] = ac.getDriverName(c.index)
+		customData["Position_" .. _idx(i) .. "_Number"] = ac.getDriverNumber(c.index)
+		customData["Position_" .. _idx(i) .. "_IsInPit"] = c.isInPit
+		customData["Position_" .. _idx(i) .. "_LapCount"] = c.lapCount
+		customData["Position_" .. _idx(i) .. "_TyreCompound"] = ac.getTyresName(c.index, c.compoundIndex)
+		if (c.isInPit == true and pitTimer[c.index] == 0) then
+			lastPitLap[c.index] = c.lapCount
 		end
-		
+		if (c.isInPit == true) then
+			pitTimer[c.index] = pitTimer[c.index] + dt
+		end
+		if (c.isInPit ~= true and pitTimer[c.index] ~= 0) then
+			lastPitTime[c.index] = pitTimer[c.index]
+			pitTimer[c.index] = 0
+			if (c.lapCount > 0) then
+				customData["Position_" .. _idx(i) .. "_LastPitTime"] = tonumber(string.format("%.1f", lastPitTime[c.index]))
+				customData["Position_" .. _idx(i) .. "_LastPitLap"] = lastPitLap[c.index]
+			end
+		end
 	end
-
 end
 
 return LeaderBoardExtension
