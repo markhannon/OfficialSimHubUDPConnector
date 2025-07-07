00e9d1fcd259661b448487e45597a1e0c9754cdd
Mark Hannon <mark.hannon@gmail.com>
2025-07-08 07:50:43 +1000

    Cleanup and remove everything except the tyre compound

--- Assetto Corsa/apps/lua/SimHubUDPConnector/extensions/LeaderBoardExtension.lua
+++ Assetto Corsa/apps/lua/SimHubUDPConnector/extensions/LeaderBoardExtension.lua
@@ -1,6 +1,8 @@
 -- Extension created by Mark Hannon <mark.hannon@gmail.com>
 --
 -- Generates leaderboard style properties for SimHub dashboards
+-- that are missing from standard data.   Currently sends tyre
+-- compound data as this is not available otherwise.
 ---@diagnostic disable: param-type-mismatch
 require("extensions.Extension")
 
@@ -13,39 +15,20 @@ function LeaderBoardExtension:new(o)
 	return o
 end
 
+-- Constant to define minimum time between updates
+local MINIMUM_TIME_BETWEEN_UPDATES = 1
+
 -- helper function to format position with two digits
 local function _idx(i)
 	return string.format("%02d", tostring(i))
 end
 
--- Lap of the last pit stop (indexed by car_id)
-local lastPitLap = {}
--- Number of seconds in the last pit stop (indexed by car_id)
-local lastPitTime = {}
--- Number of seconds in pit for this stop (indexed by car_id)
-local thisPitTime = {}
--- Number of times car has pitted (indexed by car_id)
-local pitStops = {}
-
--- Table of cars used to store car_id in sorted list
-local allCars = {}
-
-local connectedCars = {}
-local unconnectedCars = {}
-
-for i, c in ac.iterateCars() do
-	table.insert(allCars, c.index)
-	lastPitLap[c.index] = 0
-	lastPitTime[c.index] = 0
-	thisPitTime[c.index] = 0
-	pitStops[c.index] = 0
-end
-
--- Filter function to remove all non connected cars from table
-local function filterConnectedCars(allCars)
+-- helper function to split cars table into one with
+-- connected cars and one with unconnected cars
+local function classifyCars(cars)
 	local _connected = {}
 	local _unconnected = {}
-	for i, carIndex in ipairs(allCars) do
+	for i, carIndex in ipairs(cars) do
 		if (ac.getCar(carIndex).isConnected == true) then
 			table.insert(_connected, carIndex)
 		else
@@ -57,74 +40,54 @@ end
 
 -- Sort function to sort a table of car_ids by bestLapTimeMs
 -- Used in practice and qualify sessions
-local function sortCarsByBestLapTimeMs(left, right)
+local function sortCarsByTime(left, right)
 	return ac.getCar(left).bestLapTimeMs < ac.getCar(right).bestLapTimeMs
 end
 
--- Sort function to sort a table of car_ids by totalSpline.
--- Used in race sessions
-local function sortCarsByTotalSpline(left, right)
+-- Sort function to sort a table of car_ids by lapCount + splinePosition
+-- Used in race sessions to determine position order.
+local function sortCarsByPosition(left, right)
 	return (ac.getCar(left).lapCount + ac.getCar(left).splinePosition) >
-		(ac.getCar(right).lapCount + ac.getCar(right).splinePosition)
+	(ac.getCar(right).lapCount + ac.getCar(right).splinePosition)
+end
+
+-- Table of cars used to store car_id in sorted list
+-- Seperate tables for connected and unconnectedCars
+local cars = {}
+local carsConnected = {}
+local carsNotConnected = {}
+for i, car in ac.iterateCars() do
+	table.insert(cars, car.index)
 end
 
 -- Timestamp to delay expensive sort operations
 local timeSinceLastUpdate = 0
 
--- Minimum time between updates
-local minimumTimeBetweenUpdates = 0.3
-
 function LeaderBoardExtension:update(dt, customData)
 	timeSinceLastUpdate = timeSinceLastUpdate + dt
-	for i, c in ac.iterateCars() do
-		if (c.isInPitlane == true and thisPitTime[c.index] == 0) then
-			lastPitLap[c.index] = c.lapCount
-			pitStops[c.index] = pitStops[c.index] + 1
-		end
-		if (c.isInPitlane == true) then
-			thisPitTime[c.index] = thisPitTime[c.index] + dt
-		end
-		if (c.isInPitlane ~= true and thisPitTime[c.index] ~= 0) then
-			lastPitTime[c.index] = thisPitTime[c.index]
-			thisPitTime[c.index] = 0
-		end
-	end
-	if (timeSinceLastUpdate > minimumTimeBetweenUpdates) then
-		connectedCars, unconnectedCars = filterConnectedCars(allCars)
+
+	if (timeSinceLastUpdate > MINIMUM_TIME_BETWEEN_UPDATES) then
+
+		-- Create seperate tables of connected and not-connected cars
+		carsConnected, carsNotConnected = classifyCars(cars)
+
+		-- Sort the connected car table as per appropriate rules for session.
 		if (ac.getSim().raceSessionType == 3) then
-			table.sort(connectedCars, sortCarsByTotalSpline)
-			customData.LeaderBoardExtensionSession = 'Race'
+			table.sort(carsConnected, sortCarsByPosition)
 		else
-			table.sort(connectedCars, sortCarsByBestLapTimeMs)
-			customData.LeaderBoardExtensionSession = 'Practice or Qualify'
+			table.sort(carsConnected, sortCarsByTime)
 		end
 
-		for i, carIndex in ipairs(connectedCars) do
+		-- Update the tyre data for connected cars
+		for i, carIndex in ipairs(carsConnected) do
 			car = ac.getCar(carIndex)
-			customData["Position_" .. _idx(i) .. "_IsConnected"] = true
-			customData["Position_" .. _idx(i) .. "_DriverName"] = ac.getDriverName(carIndex)
-			customData["Position_" .. _idx(i) .. "_Number"] = ac.getDriverNumber(carIndex)
-			customData["Position_" .. _idx(i) .. "_IsInPit"] = car.isInPit
-			customData["Position_" .. _idx(i) .. "_LapCount"] = car.lapCount
 			customData["Position_" .. _idx(i) .. "_TyreCompound"] = ac.getTyresName(carIndex, car.compoundIndex)
-			if (thisPitTime[carIndex] == 0) then
-				customData["Position_" .. _idx(i) .. "_LastPitTime"] = math.floor(lastPitTime[carIndex]+0.5)
-				customData["Position_" .. _idx(i) .. "_LastPitLap"] = lastPitLap[carIndex]
-			end
-			customData["Position_" .. _idx(i) .. "_PitStops"] = pitStops[carIndex]
 		end
 
-		local offset = #connectedCars
-		for i, carIndex in ipairs(unconnectedCars) do
-			customData["Position_" .. _idx(i+offset) .. "_IsConnected"] = false
-			customData["Position_" .. _idx(i+offset) .. "_DriverName"] = nil
-			customData["Position_" .. _idx(i+offset) .. "_Number"] = nil
-			customData["Position_" .. _idx(i+offset) .. "_IsInPit"] = nil
-			customData["Position_" .. _idx(i+offset) .. "_LapCount"] = nil
+		-- Clear the tyre data for unconnected cars
+		local offset = #carsConnected
+		for i, carIndex in ipairs(carsNotConnected) do
 			customData["Position_" .. _idx(i+offset) .. "_TyreCompound"] = nil
-			customData["Position_" .. _idx(i+offset) .. "_LastPitTime"] = nil
-			customData["Position_" .. _idx(i+offset) .. "_LastPitLap"] = nil
-			customData["Position_" .. _idx(i+offset) .. "_PitStops"] = nil
 		end
 
 		timeSinceLastUpdate = 0
